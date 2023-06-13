befd2b65e9ed3ab3995fade9b5c6b0f8b237708f
Daniel Marshall <dpm42@duke.edu>
2023-06-13 13:34:30 -0400

    updates to tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('../../src/wmglab_neuron'))
 # -- Project information -----------------------------------------------------
 
 project = 'wmglab-neuron'
-copyright_info = '2022, Duke University'
+copyright_info = '2023, Duke University'
 
 # The full version, including alpha/beta/rc tags
 release = 'alpha'
