42680b33a86e9ae2a524f5dfd9b93b8d2ae4a030
Daniel Marshall <dpm42@duke.edu>
2024-08-01 09:10:16 -0400

    remove integration tests from coverage report

---
+++ tests/integration/__init__.py
@@ -0,0 +1,13 @@
+"""Initializer for ASCENT tests.
+
+The copyrights of this software are owned by Duke University. Please
+refer to the LICENSE and README.md files for licensing instructions. The
+source code can be found on the following GitHub repository:
+https://github.com/wmglab-duke/ascent
+"""
+
+from __future__ import annotations
+
+import sys
+
+sys.path.append('../..')
