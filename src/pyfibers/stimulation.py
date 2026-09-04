860b5a242c6b0fc5baec9825a987b137c4299476
Daniel Marshall <dpm42@duke.edu>
2026-09-04 11:49:09 -0400

    docs: update comment

--- src/pyfibers/stimulation.py
+++ src/pyfibers/stimulation.py
@@ -269,8 +269,8 @@ class Stimulation:
         :param fiber: The :class:`~pyfibers.fiber.Fiber` object for which the simulation will be configured.
         :param ap_detect_threshold: Threshold for detecting action potentials (default: -30 mV).
         """
-        # Bind time to this fiber's section so replacing a fiber does not drop the recorder
-        # https://github.com/neuronsimulator/nrn/issues/3603#issuecomment-3299874561
+        # Bind time recorder to this fiber (recorder must target a specific section)
+        # https://github.com/neuronsimulator/nrn/issues/3603
         self.time = fiber.record_time()
         # Set simulation temperature based on the fiber's temperature
         h.celsius = fiber.temperature
