3e5513711168cbfc799c9ac98dfee5d9a008f108
Daniel Marshall <68913874+thedannymarsh@users.noreply.github.com>
2026-09-22 15:05:10 -0400

    fibersec (#61)
    * fix: add fiber section to time recorder

--- src/pyfibers/stimulation.py
+++ src/pyfibers/stimulation.py
@@ -269,9 +269,9 @@ class Stimulation:
         :param fiber: The :class:`~pyfibers.fiber.Fiber` object for which the simulation will be configured.
         :param ap_detect_threshold: Threshold for detecting action potentials (default: -30 mV).
         """
-        # reassign time recorder
-        # without this, time recording can get messed up for unclear reasons
-        fiber.time = self.time = h.Vector().record(h._ref_t)
+        # Bind time recorder to this fiber (recorder must target a specific section)
+        # https://github.com/neuronsimulator/nrn/issues/3603
+        self.time = fiber.record_time()
         # Set simulation temperature based on the fiber's temperature
         h.celsius = fiber.temperature
         # Initialize the simulation to the fiber's rest potential
